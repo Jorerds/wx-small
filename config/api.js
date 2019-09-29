197db47908e94e393660de3a809f50d922c76276
company <2554981512@qq.com>
2019-09-29 15:34:08 +0800

    Initial Commit

---
+++ config/api.js
@@ -0,0 +1,5 @@
+const ApiRootUrl ='http://localhost:8016/bjboa/api/';
+
+module.exports={
+IndexUrl:ApiRootUrl, //首页数据接口
+}
---
+++ pages/index/index.js
@@ -0,0 +1,96 @@
+//index.js
+//获取应用实例
+const api=require('../../config/api.js');
+const util = require('../../utils/util.js');
+const app = getApp()
+var QQMapWX = require('../../libs/qqmap-wx-jssdk.js');
+var qqmapsdk;
+Page({
+  data: {
+    motto: 'Hello World',
+    status:1,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+  },
+  
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  bindViewNewk:function(){
+    wx.navigateTo({
+      url:'../newk/newk'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  getMap:function(){
+    wx.getLocation({
+      type: 'wgs84',
+      success: (res) => {
+        var latitude = res.latitude // 纬度
+        var longitude = res.longitude // 经度
+        console.log(res)
+      }
+    })
+    //条形码/二维码识别api
+    // wx.scanCode({
+    //   success: (res) => {
+    //     console.log(res)
+    //   }
+    // })
+  },
+  getBjboaData:function(){
+    wx.request({
+        url: 'http://localhost:8016/bjboa/api',
+        data: {},
+        method: 'GET',
+        header:{
+            'content-type':'application/json',
+            'X-Nideshop-Token': wx.getStorageSync('token')
+        },
+        success(res){
+            if(res.statusCode==200){
+                console.log(res.data)
+            }
+        }
+    })
+  }
+})
